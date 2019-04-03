4b6f50672ee7e5a86be44d9f80eb72982b340cbb
mil-neon <mil.www.25@icloud.com>
2019-04-03 17:19:29 +0900

    settei

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
   root 'messages#index'
-  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :users, only: [:edit, :update]
+  end
 end
