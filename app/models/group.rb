4b9115a05ff9888e994e2d58a9661f550178d74d
mil-neon <mil.www.25@icloud.com>
2019-04-07 00:20:25 +0900

    送信機能作成

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,21 @@
 class Group < ApplicationRecord
   has_many :group_users
+  has_many :messages
   has_many :users, through: :group_users
+
   validates :name, presence: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
+  def show_last_user
+    if (last_user = user.last).present?
+      last_user.content? ? last_user.content : ''
+    end
+  end
 end
