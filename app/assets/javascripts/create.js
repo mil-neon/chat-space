69fcc797ad2fc39bdc4bb42e911ceb6298d75192
mil-neon <mil.www.25@icloud.com>
2019-04-14 01:00:51 +0900

    非同期通信実装

---
+++ app/assets/javascripts/create.js
@@ -0,0 +1,51 @@
+$(function(){
+  function buildHTML(message){
+    var Image = (message.image) ? `<img class="lower-message__image" src="${message.image}">` : '';
+    var html = `<div class="message" data-message-id="${message.id}">
+                    <div class="message__info">
+                      <div class="message__info__talker">
+                        ${ message.user_name }
+                      </div>
+                      <div class="message__info__date">
+                        ${ message.date }
+                      </div>
+                    </div>
+                    <div class="message__text">
+                      ${ message.content }<br/><br/>
+                      ${ Image }
+                    </div>
+                  </div>`;
+    return html;
+  }
+
+  $('.message__box').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages__contents').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+    })
+
+    .always(function(){
+      $('.form__message').val('');
+      $('.hidden').val('');
+      $('.form__submit').prop('disabled', false);
+    })
+
+    .fail(function(){
+      alert('error');
+    });
+  });
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,4 @@
+json.user_name @message.user.name
+json.content @message.content
+json.image @message.image_url
+json.date @message.created_at.strftime("%Y/%m/%d %H:%M")
